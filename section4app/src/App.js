c64afae13c2910e9e6fca440a178d8d308feb1be
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-06-26 17:07:09 -0500

    Importing a css file an use it in className

--- section4app/src/App.js
+++ section4app/src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+//import './App.css';
+import "./main.css"
 
 const estilo2 = {
     boxShadow: '0 5px 3px rgba(0,0,0,0,0.5)'
@@ -12,13 +13,13 @@ const estilo = (bg = '#333') => ({
 const Li = ({children, estado}) => {
     console.log(estado)
     return (
-        <li style={{...estilo2, ...estilo('#057')}} className='clase-li'>{children}</li>
+        <li className='clase-li'>{children}</li>
     )
 }
 
 const App = () => {
   return (
-     <ul style={estilo('#750')} className='clase-css'>
+     <ul className='clase-css'>
        <Li estado='feliz'>Holi</Li>
      </ul>
   );
