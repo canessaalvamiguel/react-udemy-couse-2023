b3880ae427afcb9960b43335c4d8463a44abe078
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-06-26 17:00:45 -0500

    Use styles css

--- section4app/src/App.js
+++ section4app/src/App.js
@@ -1,16 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+const estilo2 = {
+    boxShadow: '0 5px 3px rgba(0,0,0,0,0.5)'
+}
+const estilo = (bg = '#333') => ({
+    backgroundColor: bg,
+    color: '#fff',
+    padding: '10px 15px'
+});
 
 const Li = ({children, estado}) => {
     console.log(estado)
     return (
-        <li>{children}</li>
+        <li style={{...estilo2, ...estilo('#057')}} className='clase-li'>{children}</li>
     )
 }
 
 const App = () => {
   return (
-     <ul className='clase-css'>
+     <ul style={estilo('#750')} className='clase-css'>
        <Li estado='feliz'>Holi</Li>
      </ul>
   );
