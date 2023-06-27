38fe8e9ac9236ec159ee74b3c4130c72fbe6979c
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-06-26 21:35:56 -0500

    Create first reusable component

--- section4app/src/App.js
+++ section4app/src/App.js
@@ -1,28 +1,14 @@
-//import './App.css';
-import "./main.css"
+import Button from "./Button";
 
-const estilo2 = {
-    boxShadow: '0 5px 3px rgba(0,0,0,0,0.5)'
-}
-const estilo = (bg = '#333') => ({
-    backgroundColor: bg,
-    color: '#fff',
-    padding: '10px 15px'
-});
-
-const Li = ({children, estado}) => {
-    console.log(estado)
+const App = () => {
     return (
-        <li className='clase-li'>{children}</li>
+        <div>
+            <h1>Hola Mundo</h1>
+            <Button onClick={(e) => console.log("clickeado", e)}>
+                Enviar
+            </Button>
+        </div>
     )
 }
 
-const App = () => {
-  return (
-     <ul className='clase-css'>
-       <Li estado='feliz'>Holi</Li>
-     </ul>
-  );
-}
-
-export default App;
+export default App
---
+++ section4app/src/App1.js
@@ -0,0 +1,28 @@
+//import './App.css';
+import "./main.css"
+
+const estilo2 = {
+    boxShadow: '0 5px 3px rgba(0,0,0,0,0.5)'
+}
+const estilo = (bg = '#333') => ({
+    backgroundColor: bg,
+    color: '#fff',
+    padding: '10px 15px'
+});
+
+const Li = ({children, estado}) => {
+    console.log(estado)
+    return (
+        <li className='clase-li'>{children}</li>
+    )
+}
+
+const App1 = () => {
+  return (
+     <ul className='clase-css'>
+       <Li estado='feliz'>Holi</Li>
+     </ul>
+  );
+}
+
+export default App1;
