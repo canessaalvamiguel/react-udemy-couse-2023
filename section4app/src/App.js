47c228d5cc3602ee21e07ae6355626f60283888d
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-06-26 21:38:56 -0500

    Renderizado condicional

--- section4app/src/App.js
+++ section4app/src/App.js
@@ -1,6 +1,12 @@
 import Button from "./Button";
 
 const App = () => {
+    const miVariable = true
+
+    if(miVariable){
+        return <p>Mi variable dio true</p>
+    }
+
     return (
         <div>
             <h1>Hola Mundo</h1>
